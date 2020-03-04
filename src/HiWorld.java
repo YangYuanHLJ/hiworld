c2d7ef6c7681675ba70ed2a4733a0d3f2a5f59f8
YangYuanHLJ <2357236908@qq.com>
2020-03-04 10:55:23 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,19 @@
 public class HiWorld {
-    public static void main(String args[]){
-        System.out.println("Hi Worldsdaetsdffdfdfdgdfgdfgsdgdgdsgsd");
+    public static void doia(String a ,String b,int k){
+        if(a.length() == k){
+            System.out.println(a);
+        }else{
+            for(int i = 0 ; i < b.length() ; i++){
+                String tempa = new String(a);
+                String tempb = new String(b);
+                doia(tempa+tempb.charAt(i),new StringBuffer(tempb).deleteCharAt(i).toString(),a.length()+b.length());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        doia("",s,s.length());
     }
 }
 
